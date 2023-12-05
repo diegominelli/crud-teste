ce89dac50aa04a682b93b1506beff202c50283b6
Diego Minelli <dodi@gft.com>
2023-12-05 18:51:28 -0300

    criacao e desenvolvimento do model

---
+++ src/app/models/model-clientes.ts
@@ -0,0 +1,10 @@
+export interface Cliente {
+  id: number;
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  dataNascimento: string;
+  renda: number;
+  email: string;
+  dataCadastro: string
+}
